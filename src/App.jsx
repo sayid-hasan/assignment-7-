6d17a27d73acd427d39aa3ba77e72635ad2f2f67
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-03-16 19:46:30 +0600

    added Json File

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Banner from "./Banner/Banner";
 import Navbar from "./Navbar/Navbar";
+import Recipes from "./Recipes/Recipes";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="mx-auto container md:w-[90%]">
       <Navbar></Navbar>
       <Banner></Banner>
+      <div>
+        <Recipes></Recipes>
+        <Sidebar></Sidebar>
+      </div>
     </div>
   );
 }

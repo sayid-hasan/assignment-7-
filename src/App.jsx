d376419afddeaf3005d9f1d984cf81fa775a2a02
mohammad syed hasan <syedhasanmohammad@gmail.com>
2024-03-16 13:22:23 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="mx-auto container md:w-[90%]">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
